c0da90385d93d470a7462a5efc911e19d89266e1
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-09-22 16:30:51 +0400

    added login method

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,22 @@
 import AuthContent from "../components/Auth/AuthContent";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import auth from "@react-native-firebase/auth";
 
 function LoginScreen() {
-  const loginHandler = () => {};
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+  const dispatch = useDispatch();
+
+  const loginHandler = async ({ email, password }) => {
+    setIsAuthenticating(true);
+    console.log(email, password);
+    const authResponse = await auth().createUserWithEmailAndPassword(
+      "test@test.com",
+      "Abcd@1234",
+    );
+    console.log(authResponse);
+    setIsAuthenticating(false);
+  };
 
   return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
